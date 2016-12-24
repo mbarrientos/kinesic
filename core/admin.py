3692ef1632abccca9d2ba0786ac730d05ea384d5
Miguel Barrientos <miguel.barrientos@piksel.com>
2016-12-24 12:02:57 +0100

    Added admin and django-configurations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date',)
--- core/models.py
+++ core/models.py
@@ -6,7 +6,10 @@ from django.utils.translation import ugettext_lazy as _
 class Event(models.Model):
 
     title = fields.CharField(max_length=100, verbose_name=_('Title and venue'))
-    description = fields.TextField(null=True, verbose_name=_('Long description'))
+    description = fields.TextField(blank=True, verbose_name=_('Long description'))
     date = fields.DateTimeField(verbose_name=_('Date of the event'))
-    tickets_info = fields.URLField(null=True, verbose_name=_('Link to buy tickets'))
+    tickets_info = fields.URLField(blank=True, verbose_name=_('Link to buy tickets'))
 
+    class Meta:
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
