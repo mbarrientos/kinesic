24ff6c78fb42f4f578a29b66a3f14ab4684d50b5
Miguel Barrientos <miguel.barrientos@piksel.com>
2016-12-24 12:03:14 +0100

    Added settings

---
+++ kinesic_web/settings/development.py
@@ -0,0 +1,30 @@
+"""
+Development settings
+"""
+
+import os
+
+from kinesic_web.settings.base import Base
+
+
+class Development(Base):
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 's3cr3tk3y'
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
+    # Database
+    # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        }
+    }
+
+    AUTH_PASSWORD_VALIDATORS = []
---
+++ kinesic_web/settings/production.py
@@ -0,0 +1,21 @@
+"""
+Production settings
+"""
+
+import os
+
+from kinesic_web.settings.base import Base
+
+
+class Production(Base):
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    DEBUG = False
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        }
+    }
