f57140cbf87fe51e6a3e2b81aa2e2fb60cd3e39f
Raúl Molina <raulmolina88@gmail.com>
2017-01-14 20:06:53 +0100

    added autoscroll in bio and player links

--- core/static/core/js/custom.js
+++ core/static/core/js/custom.js
@@ -1,2 +1,15 @@
 /* Add your custom JavaScript code */
 
+$("#bio-link").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#bio").offset().top
+    }, 500);
+});
+
+$("#player-link").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#player").offset().top - 70
+    }, 500);
+});
